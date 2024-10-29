b2a262c7428e194be5dbbc1a018e8d1804789eb7
stiehle <gstiehle@yahoo.de>
2024-10-29 17:35:17 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/servicetime/",
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: "parens-division",
+      },
+      // styl: {
+      //   define: {
+      //     $specialColor: new stylus.nodes.RGBA(51, 197, 255, 1),
+      //   },
+      // },
+      scss: {
+        api: "modern-compiler", // or "modern", "legacy"
+        importers: [
+          // ...
+        ],
+      },
+    },
+  },
+});
