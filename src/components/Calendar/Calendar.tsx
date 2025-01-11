3346556364cb7ee561ee51b361d40e69f7d3f8cf
stiehle <gstiehle@yahoo.de>
2025-01-11 09:17:18 +0100

    Add: Calendar Days

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -5,7 +5,7 @@ import { IoMdPersonAdd } from "react-icons/io";
 
 import { useNavigate } from "react-router-dom";
 import Serviceblock from "../Serviceblock/Serviceblock";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CalendarContext } from "../../context/CalendarContext";
 import { PiListPlusFill } from "react-icons/pi";
 
@@ -13,6 +13,7 @@ function Calendar() {
   const [days, setDays] = useState<number[]>([]);
   const navigate = useNavigate();
   const weekDayNameShort = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
+  const offsetDay = useRef(0);
 
   useEffect(() => {
     createCalendarDays(0, 10, false);
@@ -41,24 +42,62 @@ function Calendar() {
     return new Date(newDate.setDate(diff));
   }
 
+  function createNumberOfDays() {
+    const days: number[] = [2, 7, 10, 12, 16, 21];
+    function selectDay(day: number) {
+      createCalendarDays(0, day, false);
+      offsetDay.current = 0;
+    }
+
+    return days.map((d, i) => {
+      return (
+        <div key={i} className="calendar__button" onClick={() => selectDay(d)}>
+          {d}
+        </div>
+      );
+    });
+  }
+
   return (
     <IconContext.Provider value={{ size: "30px" }}>
       <CalendarContext.Provider value={{ days }}>
         <div className="calendar">
           <div className="calendar__menu">
-            <IoMdPersonAdd
-              className="calendar__add-person"
-              onClick={() => {
-                navigate("/create/person/");
-              }}
-            />
-            <PiListPlusFill
-              className="calendar__add-serviceblock"
-              onClick={() => {
-                navigate("/create/serviceblock/");
-                // navigate("/edit/serviceblock/");
-              }}
-            />
+            <div className="calendar__create">
+              <IoMdPersonAdd
+                className="calendar__add-person"
+                onClick={() => {
+                  navigate("/create/person/");
+                }}
+              />
+              <PiListPlusFill
+                className="calendar__add-serviceblock"
+                onClick={() => {
+                  navigate("/create/serviceblock/");
+                  // navigate("/edit/serviceblock/");
+                }}
+              />
+            </div>
+            <div className="calendar__number-of-days">
+              <div
+                className="calendar__one-day calendar__one-day--left"
+                onClick={() => {
+                  offsetDay.current -= 1;
+                  createCalendarDays(offsetDay.current, days.length, false);
+                }}>
+                -1
+              </div>
+              {createNumberOfDays()}
+              <div
+                className="calendar__one-day calendar__one-day--right"
+                onClick={() => {
+                  offsetDay.current += 1;
+                  createCalendarDays(offsetDay.current, days.length, false);
+                }}>
+                +1
+              </div>
+            </div>
+            <div className="calendar__setup"></div>
           </div>
           <div className="calendar__head">
             <div className="calendar__head-info">
